3316bcd2f976f8d6ac04274b630580e8ee50be29
dbasley <devinbasley@gmail.com>
2024-04-08 00:03:59 -0400

    Added script for smaller crypto

--- Bitcoin_Project.R
+++ Bitcoin_Project.R
@@ -4,32 +4,32 @@ library(tidyverse)
 
 list_coins<-crypto_list()
 
+# Cryptos we want to compare to bitcoin
+included_crypto <- c("Litecoin", "XRP", "Ethereum")
 
-#x <- crypto2::crypto_history()
+# Filter for wanted cryptocurrencies
+filtered_coins <- list_coins %>%
+  filter(name %in% included_crypto)
 
 
+
+
+# Bitcoin Data
 bitcoin <- crypto2::crypto_history(limit = 1, start_date = "20141231")
+
+# Smaller Crypto Data
+smaller_crypto <- crypto2::crypto_history(filtered_coins, start_date = "20141231", end_date = "20240327")
+
+# Write Smaller crypto to csv
+write.csv(smaller_crypto, "smaller_crypto.csv")
 
 # Data From January 1, 2015 to March 27, 2024
 write.csv(bitcoin, "bitcoin_history.csv" )
 
-#xrp <- crypto_history(coin_list = NULL, start_date = "20200101", single_id = TRUE)
-#bch<-crypto_history(coin = 'BCH', start_date = "20200101")
-#ltc<-crypto_history(coin_list = crypto_list("Litecoin", "Ethereum"), start_date = "20150101")
-#df<-rbind(btc,xrp,bch,ltc)
 
 
 
-bitcoin_close <- ts(bitcoin$close)
 
-acf(bitcoin_close)
-pacf(bitcoin_close)
-
-
-
-plot(log(bitcoin_close), main = "Bitcoin Close Price from 2015-2024",
-     ylab = "Price",
-     xlab = "Date")
 
 
 
